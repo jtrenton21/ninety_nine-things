bb3b83469268b0940a21abfcfb1d4160b2f9c52c
Alex Cheung <alex.cheung.v2@gmail.com>
2013-06-04 17:43:59 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NinetyNineThings::Application.config.secret_token = '6c4f1be0bc1b902883d20c4e18271037d5b7c869add25009a2441b29f047c42ef45f9ede7d129f1c37f5d2aca99410e588b2d468b96699fc1e03ac7cbafab15d'
