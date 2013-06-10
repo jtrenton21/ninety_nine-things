bfe2bc04561306b8078c4cc82ccf689040ab0364
Alex Cheung <alex.cheung.v2@gmail.com>
2013-06-09 20:48:12 -0700

    finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,16 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      flash[:success] = "Welcome to 99things!"
+      redirect_to @user
+    else
+      render 'new'
+    end
   end
 end
